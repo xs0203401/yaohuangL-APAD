1df59bd5ef4a459a79de9ba7b621fc0f9690557b
xs0203401 <yaohuang.liu@utexas.edu>
2018-07-22 22:22:13 -0500

    test gofish

---
+++ hw2/test_gofish.py
@@ -0,0 +1,16 @@
+import random
+import gofish
+
+def new_deck():
+    rank=["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    suit=["spades", "hearts", "diamonds", "clubs"]
+    deck = [(r, s) for r in rank for s in suit]
+    return deck
+
+if __name__ == "__main__":
+    deck = new_deck()
+    random.shuffle(deck)
+    print(deck)
+    hand = {}
+    name = "Henry"
+    gofish.drawCard(name, deck, hand)
